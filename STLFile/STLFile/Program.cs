acba62db9f3839e831903cd6f8a7720ea5ff1631
Thaniya <thaniya@kkumail.com>
2020-05-13 02:55:50 +0700

    -update facet เหลือ พิกัด

--- STLFile/STLFile/Program.cs
+++ STLFile/STLFile/Program.cs
@@ -5,7 +5,10 @@ namespace STLFile
 {
     class Program
     {
-        String []informationSTL = new String[500];
+        static string [][][]facetandvertex_int= new string[5000][][];
+
+        
+        static int number_facet = 0;
 
         static void Main(string[] args)
         {
@@ -19,10 +22,11 @@ namespace STLFile
 
             try
             {
-                fileSTL = new StreamReader("E:\\Project\\STL\\STLFile\\Test.stl");
+                fileSTL = new StreamReader("//Users//thaniya//Project//STLFile//STLFile//Test.stl"); //path in macbook
                 while ((line = fileSTL.ReadLine()) != null)
                 {
-                    Console.WriteLine(line);
+                    // Console.WriteLine(line);
+                    decodeSTLtoArray(line);
                 }
 
             }
@@ -39,5 +43,37 @@ namespace STLFile
                 }
             }
         }
+
+        static void decodeSTLtoArray(String strfromSTL)
+        {
+            if(strfromSTL.Contains("facet normal "))
+            {
+                number_facet++;
+                strfromSTL += '#';
+                String test = getBetween(strfromSTL, "facet normal ", "#");
+                String[] facet_splite = test.Split(' ');
+
+                facetandvertex_int[number_facet][0][0] = facet_splite[0];
+                facetandvertex_int[number_facet][0][1] = facet_splite[1];
+                facetandvertex_int[number_facet][0][2] = facet_splite[2];
+
+                //Console.WriteLine(facet_splite[2]);
+
+            }
+        }
+
+        public static string getBetween(string strSource, string strStart, string strEnd)
+        {
+            if (strSource.Contains(strStart) && strSource.Contains(strEnd))
+            {
+                int Start, End;
+                Start = strSource.IndexOf(strStart, 0) + strStart.Length;
+                End = strSource.IndexOf(strEnd, Start);
+                return strSource.Substring(Start, End - Start);
+            }
+
+            return "";
+        }
+
     }
 }
