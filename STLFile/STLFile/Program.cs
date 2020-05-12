1d19656f9849cd5e7b40fae2be40578c6bce96f1
Thaniya <pukavk@gmail.com>
2020-05-12 23:54:43 +0700

    Push Project

---
+++ STLFile/STLFile/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+
+namespace STLFile
+{
+    class Program
+    {
+        String []informationSTL = new String[500];
+
+        static void Main(string[] args)
+        {
+            readSTLFile();
+        }
+
+        static void readSTLFile() 
+        {
+            StreamReader fileSTL = null;
+            String line;
+
+            try
+            {
+                fileSTL = new StreamReader("E:\\Project\\STL\\STLFile\\Test.stl");
+                while ((line = fileSTL.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                }
+
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine("Error");
+            }
+
+            finally
+            {
+                if (fileSTL != null)
+                {
+                    fileSTL.Close();
+                }
+            }
+        }
+    }
+}
