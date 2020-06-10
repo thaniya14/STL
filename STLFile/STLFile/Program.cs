ac8d207875d9cd816bb436552a4b2a09d264d167
Thaniya <thaniya@kkumail.com>
2020-06-11 00:08:20 +0700

    เพิ่ม Class

--- STLFile/STLFile/Program.cs
+++ STLFile/STLFile/Program.cs
@@ -8,69 +8,114 @@ namespace STLFile
 {
     class Program
     {
-        static string [,]facetandvertex_int= new string[5,5];
-
-        static List<Setfacetandvertex> respmsg = new List<Setfacetandvertex>();
-
-        static int number_vertex = 1;
+        
 
         static void Main(string[] args)
         {
-            readSTLFile();
+            STL a = new STL();
+            a.readSTL("//Users//thaniya//Project//STLFile//STLFile//Test.stl");
+            a.details = "blah blah blah";//เพิ่มรายละเอียด
+            a.price = 250.04;//ราคา
+            // Console.WriteLine(a.JsonSTL); //แสดงJSON STL แบบแยกพิกัด
+            a.addJoint("a");//เพิ่มชิ้นส่วน
+            a.addJoint("b");
+            
+            Console.WriteLine(a.joint()[0]); //แสดง joint
+            
+
+        }
+    }
+
+
+    
+
+    class STL
+    {
+
+
+
+
+
+        private StreamReader STLFile;
+        private static int number_vertex = 1;
+        private static string[,] facetandvertex_int = new string[5, 5];
+        private static List<Setfacetandvertex> ListSTL = new List<Setfacetandvertex>();
+        public string JsonSTL;
+
+        public string details = "";
+        public double price;
+        public List<string> jointList = new List<string>();
+
+
+        public void addJoint(String jointname)
+        {
+            jointList.Add(jointname);
         }
 
-        static void readSTLFile() 
+        public string[] joint()
         {
-            StreamReader fileSTL = null;
-            String line;
+            string[] a = jointList.ToArray();
+            
+            return a;
+        }
 
+        public  void readSTL(String path)
+        {
+            String Readline;
             try
             {
-                fileSTL = new StreamReader("//Users//thaniya//Project//STLFile//STLFile//Test.stl"); //path in macbook
-                while ((line = fileSTL.ReadLine()) != null)
+                STLFile = new StreamReader(path);
+
+                while((Readline = STLFile.ReadLine())!= null)
                 {
-                    //Console.WriteLine(line);
-                    decodeSTLtoArray(line);
-                    //check_facetandvertex_array()   -> เขียนฟังชั่นนี้ต่อ
-
-
+                    decodeSTLtoArray(Readline);
                 }
-
             }
-            catch (FileNotFoundException e)
+            catch(FileNotFoundException e)
             {
-                Console.WriteLine("Error");
+                Console.WriteLine("Error at Class STL : can't read file from path");
             }
-
             finally
             {
-                if (fileSTL != null)
+                if (STLFile != null)
                 {
-                    fileSTL.Close();
+                    STLFile.Close();
                 }
 
                 try
                 {
-                    string sJSONResponse = JsonConvert.SerializeObject(respmsg);
+                     JsonSTL = JsonConvert.SerializeObject(ListSTL);
 
-                   // Console.WriteLine(sJSONResponse);
+                     //Console.WriteLine(JsonSTL);
 
-                   // System.IO.File.WriteAllText(@"\Users\thaniya\Project", sJSONResponse);  - >>> create JSON File 
+                    // System.IO.File.WriteAllText(@"\Users\thaniya\Project", sJSONResponse);  - >>> create JSON File 
                 }
                 catch
                 {
-                    Console.WriteLine("can't write");
+                    Console.WriteLine("Error at Class STL : can't create JSON Object ");
                 }
 
             }
         }
 
-        
+
+        public static string getBetween(string strSource, string strStart, string strEnd)
+        {
+            if (strSource.Contains(strStart) && strSource.Contains(strEnd))
+            {
+                int Start, End;
+                Start = strSource.IndexOf(strStart, 0) + strStart.Length;
+                End = strSource.IndexOf(strEnd, Start);
+                return strSource.Substring(Start, End - Start);
+            }
+
+            return "";
+        }
 
 
         static void decodeSTLtoArray(String strfromSTL)
         {
-            
+
 
             if (strfromSTL.Contains("facet normal "))
             {
@@ -79,21 +124,21 @@ namespace STLFile
                 {
                     number_vertex = 1;
 
-                    respmsg.Add(new Setfacetandvertex
+                    ListSTL.Add(new Setfacetandvertex
                     {
                         facet_x = facetandvertex_int[0, 0],
-                        facet_y = facetandvertex_int[0,1],
-                        facet_z = facetandvertex_int[0,2],
-                        vertex1_x = facetandvertex_int[1,0],
-                        vertex1_y = facetandvertex_int[1,1],
-                        vertex1_z = facetandvertex_int[1,2],
-                        vertex2_x = facetandvertex_int[2,0],
-                        vertex2_y = facetandvertex_int[2,1],
-                        vertex2_z = facetandvertex_int[2,2],
-                        vertex3_x = facetandvertex_int[3,0],
-                        vertex3_y = facetandvertex_int[3,1],
-                        vertex3_z = facetandvertex_int[3,2]
-                    }) ;
+                        facet_y = facetandvertex_int[0, 1],
+                        facet_z = facetandvertex_int[0, 2],
+                        vertex1_x = facetandvertex_int[1, 0],
+                        vertex1_y = facetandvertex_int[1, 1],
+                        vertex1_z = facetandvertex_int[1, 2],
+                        vertex2_x = facetandvertex_int[2, 0],
+                        vertex2_y = facetandvertex_int[2, 1],
+                        vertex2_z = facetandvertex_int[2, 2],
+                        vertex3_x = facetandvertex_int[3, 0],
+                        vertex3_y = facetandvertex_int[3, 1],
+                        vertex3_z = facetandvertex_int[3, 2]
+                    });
                 }
 
 
@@ -101,23 +146,23 @@ namespace STLFile
                 String test = getBetween(strfromSTL, "facet normal ", "#");
                 String[] facet_split = test.Split(" ");
 
-             
-                
-                facetandvertex_int[0,0] = facet_split[0];
-                facetandvertex_int[0,1] = facet_split[1];
-                facetandvertex_int[0,2] = facet_split[2];
+
+
+                facetandvertex_int[0, 0] = facet_split[0];
+                facetandvertex_int[0, 1] = facet_split[1];
+                facetandvertex_int[0, 2] = facet_split[2];
 
 
 
-               
+
 
             }
-            else if(strfromSTL.Contains("vertex "))
+            else if (strfromSTL.Contains("vertex "))
             {
                 strfromSTL += '#';
                 String test = getBetween(strfromSTL, "vertex ", "#");
-                String []vertex_split = test.Split(" ");
-                
+                String[] vertex_split = test.Split(" ");
+
                 facetandvertex_int[number_vertex, 0] = vertex_split[0];
                 facetandvertex_int[number_vertex, 1] = vertex_split[1];
                 facetandvertex_int[number_vertex, 2] = vertex_split[2];
@@ -131,6 +176,7 @@ namespace STLFile
             public List<Setfacetandvertex> Response { get; set; }
         }
 
+       
 
 
         public class Setfacetandvertex
@@ -151,25 +197,9 @@ namespace STLFile
 
         }
 
-        
-
-
-
-
-
-
-        public static string getBetween(string strSource, string strStart, string strEnd)
-        {
-            if (strSource.Contains(strStart) && strSource.Contains(strEnd))
-            {
-                int Start, End;
-                Start = strSource.IndexOf(strStart, 0) + strStart.Length;
-                End = strSource.IndexOf(strEnd, Start);
-                return strSource.Substring(Start, End - Start);
-            }
-
-            return "";
-        }
-
     }
+
+    
+        
+    
 }
